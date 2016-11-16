3696eb192e6f85643571679821a8d1b773079a6d
Tobias Dirnhuber <tdirnhuber@hotmail.co.uk>
2016-11-16 01:05:09 +0000

    using cobb-douglas utilities

--- walras.py
+++ walras.py
@@ -1,5 +1,8 @@
+import numpy as np
 from itertools import product
-from random import sample, randint
+from random import sample, randint, random
+
+
 import matplotlib.pyplot as plt
 
 class Trade():
@@ -17,19 +20,22 @@ class Trade():
             size = self.size,
             mrs = self.joint_mrs))
 
-        
-        
+    
+    
 class Trader():
     """Trader class"""
 
-    def __init__(self, name, alpha, allocation=(0,0)):
+    def __init__(self, name, preference, allocation=(0,0)):
         self.name = name
         self.preference = preference
         self.allocation = allocation
 
     def mrs(self):
-        l1,l2 = self.preference
-        return l1/l2
+        alpha = self.preference
+        x1, x2 = self.allocation
+        if x1 == 0:
+            return 10000
+        return -alpha*x2/((1-alpha)*x1)
 
     def change_allocation(self, delta):
         self.allocation  = (self.allocation[0] + delta[0],
@@ -39,10 +45,11 @@ class Trader():
         return (self.mrs() * other.mrs()) ** 0.5
 
     def get_size(self, other, joint_mrs):
+       # We should change this to used a closed form calculation of the maximum size
        if self.mrs() > other.mrs():
-           return min(joint_mrs * self.allocation[1], other.allocation[0])
+           return min(joint_mrs * self.allocation[1], other.allocation[0])/2
        elif self.mrs() < other.mrs():
-           return -min(joint_mrs * other.allocation[1], self.allocation[0])
+           return -min(joint_mrs * other.allocation[1], self.allocation[0])/2
        else:
            return 0
 
@@ -64,21 +71,18 @@ def trade_random(traders):
     return(a.trade(b))
 
 def random_traders(n):
-    return [Trader(i,(randint(1,10),randint(1,10)),
+    return [Trader(i, random(),
                       (randint(1,10),randint(1,10))) for i in range(n)]
 
 if __name__ == "__main__":
-    #i = Trader("I", (1, 8), (1, 1))
-    #j = Trader("J", (2, 1), (1, 1))
-    #k = Trader("K", (9, 2), (1, 1))
-    #print(i.trade(j))
-    #print(i.trade(k))
-    #print(j.trade(k))
-    
     sizes = []
-    traders = random_traders(5)
-    for i in range(1000):
+    traders = random_traders(100)
+    for i in range(4000):
         trade = trade_random(traders)
         sizes.append(abs(trade.size))
+    sizes = [[0,1][a>0.1] for a in sizes]
+    sizes = zip(*[sizes[n:] for n in range(25)])
+    sizes = [sum(a) for a in sizes]
+             
     plt.plot(sizes)
     plt.show()
