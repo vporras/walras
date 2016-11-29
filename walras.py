ae2f1c0edb09231c9984df9ec5e035f4eb83e213
Victor Porras <vporras@ymail.com>
2016-11-29 17:56:41 +0000

    adding command line args and better graphing

--- walras.py
+++ walras.py
@@ -2,6 +2,8 @@ import numpy as np
 from itertools import product
 from random import sample, randint, random
 import matplotlib.pyplot as plt
+import argparse
+from enum import Enum
 
 class Trade():
     
@@ -17,71 +19,120 @@ class Trade():
             origin = self.origin,
             size = self.size,
             mrs = self.joint_mrs))
-    
+
+class Dir(Enum):
+    """Direction of trade in terms of good 1"""
+    buy = 1
+    sell = -1
     
 class Trader():
     """Trader class"""
 
-    def __init__(self, name, preference, allocation=(0,0)):
+    def __init__(self, name, preference, allocation):
         self.name = name
         self.preference = preference
-        self.allocation = allocation
+        self.allocs = [allocation]
+        self.alloc = allocation
 
     def mrs(self):
         alpha = self.preference
-        x1, x2 = self.allocation
+        x1, x2 = self.alloc
         if x1 == 0:
             return 10000
-        return -alpha*x2/((1-alpha)*x1)
+        return abs(-alpha*x2/((1-alpha)*x1))
+
+    def plot(self):
+        alpha = self.preference
+        xlist = np.linspace(0, 10.0, 100)
+        ylist = np.linspace(0, 10.0, 100)
+        X1, X2 = np.meshgrid(xlist, ylist)
+        U = X1**alpha + X2**(1-alpha)
+        plt.figure()
+        cp = plt.contour(X1, X2, U)
+        plt.clabel(cp, inline=True, fontsize=10)
+        plt.title("alpha = %f" % alpha)
+        plt.xlabel("Good 1")
+        plt.ylabel("Good 2")
+
+        x1, x2 = self.alloc
+        plt.plot([x1], [x2], "o", label="current")
+        x1s, x2s = zip(*self.allocs)
+        plt.plot(x1s, x2s, ".-")
+
+        plt.show()
 
     def change_allocation(self, delta):
-        self.allocation  = (self.allocation[0] + delta[0],
-                             self.allocation[1] + delta[1])
+        self.alloc = (self.alloc[0] + delta[0],
+                              self.alloc[1] + delta[1])
+        self.allocs.append(self.alloc)
 
     def joint_mrs(self, other):
         return (self.mrs() * other.mrs()) ** 0.5
 
-    def get_size(self, other, joint_mrs):
+    def get_size(self, other, joint_mrs, abs_size):
        # We should change this to used a closed form calculation of the maximum size
+       # If it's fixed, it needs to be scaled according to the good
        if self.mrs() > other.mrs():
-           return min(joint_mrs * self.allocation[1], other.allocation[0])/2
+           return min(abs_size, joint_mrs * self.alloc[1], other.alloc[0])
        elif self.mrs() < other.mrs():
-           return -min(joint_mrs * other.allocation[1], self.allocation[0])/2
+           return -min(abs_size, joint_mrs * other.alloc[1], self.alloc[0])
        else:
            return 0
 
-    def trade(self, other):
+    def trade(self, other, abs_size):
         joint_mrs = self.joint_mrs(other)
-        size = self.get_size(other, joint_mrs)
+        size = self.get_size(other, joint_mrs, abs_size)
         self.change_allocation((size, -size / joint_mrs))
         other.change_allocation((-size, size / joint_mrs))
         return Trade(self, other, size, joint_mrs)
 
     def __str__(self):
-        return ("Name:{name},\n preference=({preference}),\n allocation=({allocation})".format(
+        return ("Name:{name},\n alpha=({preference}),\n alloc=({alloc})".format(
             name = self.name,
             preference = self.preference,
-            allocation = self.allocation))
+            alloc = self.alloc))
 
-def trade_random(traders):
+def trade_random(traders, trade_size):
     a,b = sample(traders,2)
-    return(a.trade(b))
+    return(a.trade(b, trade_size))
 
 def random_traders(n):
-    return [Trader(i, random(),
-                      (randint(1,10),randint(1,10))) for i in range(n)]
+    return [Trader(i, random(), (randint(1,10),randint(1,10)))
+            for i in range(n)]
 
-if __name__ == "__main__":
+def mrs_range(traders):
+    # currently centralised, not distributed
+    # could be calculated incrementally
+    mrss = [t.mrs() for t in traders]
+    return max(mrss) - min(mrss)
+
+def run(num_traders, trade_size):
     sizes = []
-    traders = random_traders(100)
-    for i in range(4000):
-        trade = trade_random(traders)
+    traders = random_traders(num_traders)
+    while (mrs_range(traders) > 0.01):
+        trade = trade_random(traders, trade_size)
         sizes.append(abs(trade.size))
     sizes = [[0,1][a>0.1] for a in sizes]
-    sizes = zip(*[sizes[n:] for n in range(25)])
+    bucket_size = 25 
+    sizes = zip(*[sizes[n:] for n in range(bucket_size)])
     sizes = [sum(a) for a in sizes]
+
+    mrss = [t.mrs() for t in traders]
+    print(mrss)
+    
+    for t in traders:
+        t.plot()
+
              
-    plt.plot(sizes)
-    plt.show()
+    # plt.plot(sizes)
+    # plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num_traders", type=int)
+    parser.add_argument("-s", "--trade_size", type=float, default=1.0,
+                        help="size of trade (default: 1.0)")
+    args = parser.parse_args()
+    run(args.num_traders, args.trade_size)
 
 
