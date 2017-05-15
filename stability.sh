b68be328fb0e12b1a6795304f694ca49957cd8c5
Victor Porras <vporras@ymail.com>
2017-05-15 15:43:52 +0100

    stab

--- stability.sh
+++ stability.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-stability checks
 echo stable_conv_simple
 python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --trader-file traders_conv.txt -e stable_conv_simple
 echo stable_div_simple
