c6d906e058b046176a6f325fbd2f01fe1bd9b73c
Victor Porras <vporras@ymail.com>
2017-05-15 15:23:54 +0100

    stability tests

---
+++ stability.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+stability checks
+echo stable_conv_simple
+python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --trader-file traders_conv.txt -e stable_conv_simple
+echo stable_div_simple
+python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --trader-file traders_div.txt -e stable_div_simple
+echo stable_conv_backtrack
+python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 -trader-file traders_conv.txt -e stable_conv_backtrack 
