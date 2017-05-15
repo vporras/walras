2f719261542ae3235b546dc59daafde5a392fdb9
Victor Porras <vporras@ymail.com>
2017-05-15 19:02:25 +0100

    exps

--- stability.sh
+++ stability.sh
@@ -5,4 +5,4 @@ python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --trader-file tr
 echo stable_div_simple
 python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --trader-file traders_div.txt -e stable_div_simple
 echo stable_conv_backtrack
-python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 -trader-file traders_conv.txt -e stable_conv_backtrack 
+python3 walras.py -r 500 -n 100 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 --trader-file traders_conv.txt -e stable_conv_backtrack 
