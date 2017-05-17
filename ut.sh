2f73126e46b987743dc5ec23a3390e73f29d83a7
Victor Porras <vporras@ymail.com>
2017-05-17 03:36:18 +0100

    ut

--- ut.sh
+++ ut.sh
@@ -1,10 +1,10 @@
 echo "util_cd_10"
-python3 walras.py -m 0.0000001 -n 100 -r 500 -u -s 0 -t 20 -p 0 -v 3 --finish-count 10 -e util_cd_10 
+python3 walras.py -m 0.0000001 -n 100 -r 500 -u nogradient -s 0 -t 20 -p 0 -v 3 --finish-count 10 -e util_cd_10 
 echo "util_cd_20"
-python3 walras.py -m 0.0000001 -n 100 -r 500 -u -s 0 -t 20 -p 0 -v 3 --finish-count 20 -e util_cd_20 
+python3 walras.py -m 0.0000001 -n 100 -r 500 -u nogradient -s 0 -t 20 -p 0 -v 3 --finish-count 20 -e util_cd_20 
 echo "util_cd_30"
-python3 walras.py -m 0.0000001 -n 100 -r 500 -u -s 0 -t 20 -p 0 -v 3 --finish-count 30 -e util_cd_30 
+python3 walras.py -m 0.0000001 -n 100 -r 500 -u nogradient -s 0 -t 20 -p 0 -v 3 --finish-count 30 -e util_cd_30 
 echo "util_cd_40"
-python3 walras.py -m 0.0000001 -n 100 -r 500 -u -s 0 -t 20 -p 0 -v 3 --finish-count 40 -e util_cd_40 
+python3 walras.py -m 0.0000001 -n 100 -r 500 -u nogradient -s 0 -t 20 -p 0 -v 3 --finish-count 40 -e util_cd_40 
 echo "util_cd_50"
-python3 walras.py -m 0.0000001 -n 100 -r 500 -u -s 0 -t 20 -p 0 -v 3 --finish-count 50 -e util_cd_50 
+python3 walras.py -m 0.0000001 -n 100 -r 500 -u nogradient -s 0 -t 20 -p 0 -v 3 --finish-count 50 -e util_cd_50 
