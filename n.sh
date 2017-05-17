24510817fe3117d1f1b124c67aeb22e0e730aca1
Victor Porras <vporras@ymail.com>
2017-05-17 02:24:35 +0000

    n exps

--- n.sh
+++ n.sh
@@ -14,5 +14,3 @@ echo n_bt_320
 python3 walras.py -r 500 -n 320  -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 -e n_bt_320
 echo n_bt_640
 python3 walras.py -r 500 -n 640  -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 -e n_bt_640
-echo n_bt_1280
-python3 walras.py -r 500 -n 1280 -m 0.0001 -s 0 -v 1 -p 0 -t 100 --backtrack-prob 0.5 -b 5 25 100 -e n_bt_1280
